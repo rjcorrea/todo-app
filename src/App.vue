09778ef024cd85500dff9a8a2cc7772ad786f8fd
Robin <robin.correa@bizmates.ph>
2019-10-11 15:05:00 +0800

    Add bottom navigation.

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,23 @@
     <v-content>
       <router-view></router-view>
     </v-content>
+
+    <v-bottom-navigation v-model="bottomNav">
+      <v-btn to="/" value="recent">
+        <span>Login</span>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+
+      <v-btn to="/todos" value="favorites">
+        <span>Todos</span>
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+
+      <v-btn value="nearby">
+        <span>Nearby</span>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
