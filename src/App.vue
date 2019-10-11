75b6d65cec06b084990c44f5930496ea09f4b52d
Robin <robin.correa@bizmates.ph>
2019-10-11 16:35:45 +0800

    Add Sidebar drawer and Footer

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <template>
   <v-app>
-    <v-app-bar app v-if="$route.path != '/'">
-      <v-toolbar-title class="headline text-uppercase">
-        <span>TODO</span>
-        <span class="font-weight-light">APP</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn text to="/">
-        <v-avatar class="mr-2" color="primary" size="35">
-          <span class="white--text">RJ</span>
-        </v-avatar>
-        <span class="hidden-xs-only">Robin</span>
-      </v-btn>
-    </v-app-bar>
+    <template v-if="$route.path != '/'">
+      <AppSidebar :drawer="drawer" />
+      <v-app-bar app>
+        <v-app-bar-nav-icon @click="drawer =!drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title class="headline text-uppercase">
+          <span>TODO</span>
+          <span class="font-weight-light">APP</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text to="/">
+          <v-avatar class="mr-2" color="primary" size="35">
+            <span class="white--text">RJ</span>
+          </v-avatar>
+          <span class="hidden-xs-only">Robin</span>
+        </v-btn>
+      </v-app-bar>
+    </template>
     <v-content>
       <router-view></router-view>
     </v-content>
+    <AppFooter />
   </v-app>
 </template>
 
 <script>
+import AppSidebar from "@/components/AppSidebar";
+import AppFooter from "@/components/AppFooter";
 export default {
   name: "App",
-  data: () => ({
-    //
-  })
+  components: { AppSidebar, AppFooter },
+  data() {
+    return {
+      drawer: true
+    };
+  }
 };
 </script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-app-bar color="#34495e" fixed app>
+      <v-app-bar-nav-icon class="white--text" @click="drawer =!drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="white--text">My Vue App 1</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+      <v-toolbar-items>
+        <v-btn class="white--text" text>About</v-btn>
+        <v-btn class="white--text" text>Contact</v-btn>
+        <v-btn class="white--text" text>Sign In</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
+</script>
