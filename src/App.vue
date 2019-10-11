a8a251a05f2769a2b237d81d6b41561487b9028a
rjcorrea <rjcor21@gmail.com>
2019-10-11 13:40:10 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/" exact>Home 1234</router-link>|
+      <router-link to="/" exact>Home 12345</router-link>|
       <router-link to="/todos" exact>Todos</router-link>
     </nav>
     <router-view></router-view>
