0f7cb76ae60e8a9f82ff93eb8abc8458c2d9d4d3
Robin <robin.correa@bizmates.ph>
2019-10-11 15:52:48 +0800

    Fix the user icon

--- src/App.vue
+++ src/App.vue
@@ -6,38 +6,24 @@
         <span class="font-weight-light">APP</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <span class="mr-2">Latest Release</span>
+      <v-btn text to="/">
+        <v-avatar class="mr-2" color="primary" size="35">
+          <span class="white--text">RJ</span>
+        </v-avatar>
+        <span class="hidden-xs-only">Robin</span>
       </v-btn>
     </v-app-bar>
     <v-content>
       <router-view></router-view>
     </v-content>
-
-    <v-bottom-navigation v-model="bottomNav">
-      <v-btn to="/" value="recent">
-        <span>Login</span>
-        <v-icon>mdi-account</v-icon>
-      </v-btn>
-
-      <v-btn to="/todos" value="favorites">
-        <span>Todos</span>
-        <v-icon>mdi-pencil</v-icon>
-      </v-btn>
-
-      <v-btn value="nearby">
-        <span>Nearby</span>
-        <v-icon>mdi-map-marker</v-icon>
-      </v-btn>
-    </v-bottom-navigation>
   </v-app>
 </template>
 
 <script>
 export default {
   name: "App",
-  data() {
-    return {};
-  }
+  data: () => ({
+    //
+  })
 };
 </script>
