54ed4cfbc35765b1e0472c67e83d57b50437bf3a
Robin <robin.correa@bizmates.ph>
2019-10-14 15:07:26 +0800

    Edit login page.

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,29 +1,35 @@
 <template>
   <v-app>
     <v-layout align-center justify-center row fill-height>
-      <v-card width="450">
-        <v-card-title>
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-divider></v-divider>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-            <v-text-field
-              :type="showPassword ? 'text' : 'password'"
-              label="Password"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-row class="fill-height" align="center" justify="center">
+        <v-col cols="12" sm="6" md="5" lg="4" xl="3">
+          <v-container>
+            <v-card>
+              <v-card-title>
+                <h1 class="display-1">Login</h1>
+              </v-card-title>
+              <v-divider></v-divider>
+              <v-card-text>
+                <v-form>
+                  <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+                  <v-text-field
+                    :type="showPassword ? 'text' : 'password'"
+                    label="Password"
+                    prepend-icon="mdi-lock"
+                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="showPassword = !showPassword"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-container>
+        </v-col>
+      </v-row>
     </v-layout>
   </v-app>
 </template>
